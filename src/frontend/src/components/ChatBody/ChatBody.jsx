28787292863c6583a7c9fb38cf5f0be29d3d44f5
Mihir Mehta <mehtam2@uci.edu>
2025-11-21 19:59:23 -0800

    added auto scroll when a new message appears

--- src/frontend/src/components/ChatBody/ChatBody.jsx
+++ src/frontend/src/components/ChatBody/ChatBody.jsx
@@ -1,7 +1,18 @@
 import './ChatBody.css';
 import ChatMessage from '../ChatMessage/ChatMessage';
+import { useRef, useEffect } from 'react';
+
 
 export default function ChatBody({ messages }) {
+    const messagesEndRef = useRef(null);
+    
+    //created an empty div after all the chat messages
+    //when the messages change, scroll to that empty div, which consequently scrolls to the bottom of the chatbox
+    //useEffect will only run after a render, so messagesEndRef will never be null
+    useEffect(() => {
+        messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' });
+    }, [messages]);
+    
     return (
         <div className="chat-body">
             <div className = "chat-body-messages">
@@ -12,6 +23,7 @@ export default function ChatBody({ messages }) {
                         content={message.content}
                     />
                 ))}
+                <div ref={messagesEndRef} />
             </div>
         </div>
     );
